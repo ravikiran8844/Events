b252a8e9f84b5d4ee931fb2922b4a0f0a0908169
root <ravi.kiran8844@.com>
2022-10-09 05:09:19 +0000

    first commit

---
+++ src/components/EventItem/index.js
@@ -0,0 +1,25 @@
+import './index.css'
+
+const EventItem = props => {
+  const {eventsList, handleClick} = props
+  const {imageUrl, name, location, registrationStatus} = eventsList
+
+  const getEventDetails = () => {
+    console.log('clicked')
+    handleClick(registrationStatus)
+  }
+
+  return (
+    <div className="col-6">
+      <li>
+        <button type="button" onClick={getEventDetails}>
+          <img className="w-100" src={imageUrl} alt="event" />
+        </button>
+        <p>{name}</p>
+        <p>{location}</p>
+      </li>
+    </div>
+  )
+}
+
+export default EventItem
